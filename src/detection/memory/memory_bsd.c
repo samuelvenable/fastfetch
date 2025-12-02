7b4ec157f18082c0e5242d47e0abb73db5eafabc
high on tantor <josephvenable@verizon.net>
2025-12-02 01:59:27 -0500

    Update memory_bsd.c

--- src/detection/memory/memory_bsd.c
+++ src/detection/memory/memory_bsd.c
@@ -4,10 +4,10 @@
 const char* ffDetectMemory(FFMemoryResult* ram)
 {
     // vm.stats.vm.* are int values
-    ram->bytesTotal = (uint64_t) ffSysctlGetInt("vm.stats.vm.v_page_count", 0);
+    ram->bytesTotal = (uint64_t) ffSysctlGetInt("vm.stats.vm.v_page_count", 0) * instance.state.platform.sysinfo.pageSize;
     int32_t pagesFree = ffSysctlGetInt("vm.stats.vm.v_free_count", 0);
 
-    ram->bytesUsed = (ram->bytesTotal - (uint64_t) pagesFree) * instance.state.platform.sysinfo.pageSize;
+    ram->bytesUsed = ram->bytesTotal - (uint64_t) pagesFree * instance.state.platform.sysinfo.pageSize;
 
     return NULL;
 }
